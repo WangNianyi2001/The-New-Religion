1eb56a50ccc5fc368b7d3fb338628ec5395c0913
Nianyi Wang <Wangnianyi2001@outlook.com>
2022-10-30 12:58:53 +0800

    Upload

---
+++ Assets/Modules/GameManager.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Canvas), typeof(StandaloneInputModule), typeof(AudioListener))]
+public class GameManager : MonoBehaviour {
+}
